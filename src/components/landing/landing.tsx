a88eb95f9175714259b9d8f0a8b56c43fe666556
Binh Duc Tran <binhtran432k@gmail.com>
2024-08-07 17:13:53 +0700

    feat: enhance cool background with threejs

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -1,16 +1,26 @@
-import { component$ } from "@builder.io/qwik";
-import BackgroundImage from "~/assets/background.jpg?jsx";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import backgroundImage from "~/assets/background.jpg";
+import coolBackground from "~/utils/cool-background";
 import Header from "../header/header";
 import Intro from "../intro/intro";
-import Social from "../social/social";
 import ScrollDown from "../scroll-down/scroll-down";
+import Social from "../social/social";
 
 export default component$(() => {
+  const imageRef = useSignal<Element>();
+
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(() => {
+    if (imageRef.value) {
+      coolBackground(imageRef.value, backgroundImage);
+    }
+  });
+
   return (
     <div class="relative flex min-h-screen flex-col">
-      <BackgroundImage
-        class="absolute left-0 top-0 z-[-1] h-full w-full object-cover brightness-50"
-        alt="Background"
+      <div
+        class="absolute left-0 top-0 z-[-1] h-full w-full overflow-hidden"
+        ref={imageRef}
       />
       <Header />
       <div class="flex grow flex-col flex-wrap justify-between">
