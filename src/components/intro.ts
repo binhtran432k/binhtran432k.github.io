993f0042ff9d7da4b4b4e62653b3c22645f1197b
Binh Tran <binhtran432k@gmail.com>
2024-11-13 06:39:05 +0000

    feat(landing): add social

--- src/components/intro.ts
+++ src/components/intro.ts
@@ -1,6 +1,7 @@
 import { env } from "mini-van-plate/shared";
 
 import { Background } from "./background.js";
+import { Social } from "./social.js";
 
 export const Intro = () => {
 	const { div, h1, h2, h3, span } = env.van.tags;
@@ -15,5 +16,6 @@ export const Intro = () => {
 		h1("BINH TRAN"),
 		h2("Self Studied Developer"),
 		h2("from Vietnam"),
+		Social(),
 	);
 };
---
+++ src/components/social.ts
@@ -0,0 +1,20 @@
+import { env } from "mini-van-plate/shared";
+
+const socials = {
+	linkedin: "https://www.linkedin.com/in/binhtran432k",
+	github: "https://github.com/binhtran432k",
+	instagram: undefined,
+	x: "https://twitter.com/binhtran432k",
+	facebook: "https://www.facebook.com/binhtran432k",
+	youtube: "https://www.youtube.com/@binhtran432k",
+} as const;
+
+export const Social = () => {
+	const { div, a, span } = env.van.tags;
+	return div(
+		{ class: "social" },
+		Object.entries(socials)
+			.filter(([, x]) => Boolean(x))
+			.map(([label, href]) => a({ href, target: "_blank" }, span({ class: `bi-${label}` }))),
+	);
+};
